d84a50ec68d28d2c5422b9d0b33321f71a2e6bbf
redstonetrail <joe.bearchell@gmail.com>
2026-02-20 08:59:44 +0000

    package for nix using zig2nix

--- flake.nix
+++ flake.nix
@@ -2,20 +2,40 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/bce5fe2bb998488d8e7e7856315f90496723793c";
     flake-utils.url = "github:numtide/flake-utils";
+
+    zig2nix = {
+      url = "github:Cloudef/zig2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      zig2nix,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        zig-env = zig2nix.outputs.zig-env.${system} {
+          zig = zig2nix.outputs.packages.${system}.zig-0_15_2;
+        };
       in
-      {
+      rec {
+        packages.default = zig-env.package rec {
+          src = zig-env.pkgs.lib.cleanSource ./.;
+
+          zigBuildFlags = [ "-Doptimize=ReleaseFast" ];
+        };
+
+        apps.build = zig-env.app [ ] "zig build \"$@\"";
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
             pkgs.zig
